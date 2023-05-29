a42d2819eb05ff0abaa4487ea9685f8864aa5ad4
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-05-29 21:06:25 +0900

    🌈feat : 이미지 케루셀 추가

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -2,16 +2,16 @@ import Layout from 'components/Layout/Layout'
 import LeftSection from './components/LeftSection'
 import MiddleSection from './components/MiddleSection'
 import RightSection from './components/RightSection/RightSection'
-// import PostLists from './PostLists/PostLists'
+import PostLists from './PostLists/PostLists'
+// import PostLists2 from './PostLists/_PostLists'
 import RightInnerBanner from './components/RightSection/Banner'
-import PostLists2 from './PostLists/_PostLists'
 
 const MainPage = () => {
 	return (
 		<Layout>
 			<LeftSection>leftSection</LeftSection>
 			<MiddleSection>
-				<PostLists2 />
+				<PostLists />
 			</MiddleSection>
 			<RightSection>
 				<RightInnerBanner></RightInnerBanner>
